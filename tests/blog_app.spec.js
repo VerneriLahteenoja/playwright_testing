524d00db08839a1d07d96563bfc4b7584c328563
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-04-07 23:33:41 +0300

    create test if can log in

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -13,3 +13,14 @@ test('can open log in form', async ({ page }) => {
   await expect(page.getByText('password')).toBeVisible()
 })
 
+test('can log in', async ({ page }) => {
+  await page.goto('http://localhost:5173')
+  await page.getByRole('button', { name: 'Log in' }).click()
+
+  await page.getByTestId('username').fill('superuser')
+  await page.getByTestId('password').fill('superpass')
+  
+  await page.getByRole('button', { name: 'login' }).click()
+
+  await expect(page.getByText('Super Tester logged in')).toBeVisible()
+})
