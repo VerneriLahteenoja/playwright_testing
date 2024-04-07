c5fef3a6676bd77e978054b1c079163864e3aa4a
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-04-07 23:29:35 +0300

    create test open login form

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -2,7 +2,14 @@ const { test, expect } = require('@playwright/test')
 
 test('front page can be opened', async ({ page }) => {
   await page.goto('http://localhost:5173')
-  const locator = await page.getByText('Blogs')
+  const locator = page.getByText('Blogs')
   await expect(locator).toBeVisible()
-
 })
+
+test('can open log in form', async ({ page }) => {
+  await page.goto('http://localhost:5173')
+  await page.getByRole('button', { name: 'Log in' }).click()
+  await expect(page.getByText('username')).toBeVisible()
+  await expect(page.getByText('password')).toBeVisible()
+})
+
