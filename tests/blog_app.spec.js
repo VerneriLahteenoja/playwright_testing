4031e11d51d6878ad43b61cc188bb8cfbbd5a676
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-04-08 20:39:27 +0300

    create logged in tests

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -24,6 +24,20 @@ describe('Blog app', () => {
       await expect(page.getByText('username')).toBeVisible()
       await expect(page.getByText('password')).toBeVisible()
     })
+    test('fail to log in', async ({ page }) => {
+      await page.getByRole('button', { name: 'Log in' }).click()
+      await page.getByTestId('username').fill('notUser')
+      await page.getByTestId('password').fill('notPass')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('Wrong username or password')).toBeVisible()
+    })
+    test('can log in', async ({ page }) => {
+      await page.getByRole('button', { name: 'Log in' }).click()
+      await page.getByTestId('username').fill('superuser')
+      await page.getByTestId('password').fill('superpass')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('Super Tester logged in')).toBeVisible()
+    })
   })
   
   describe('when logged in', () => {
@@ -33,9 +47,6 @@ describe('Blog app', () => {
       await page.getByTestId('password').fill('superpass')
       await page.getByRole('button', { name: 'login' }).click()
     })
-    test('can log in', async ({ page }) => {
-      await expect(page.getByText('Super Tester logged in')).toBeVisible()
-    })
     test('can create new blog', async ({ page }) => {
       await page.getByRole('button', { name: 'Add new blog' }).click()
       await page.getByTestId('title').fill('Playwright test blog')
@@ -44,6 +55,27 @@ describe('Blog app', () => {
       await page.getByRole('button', { name: 'Add' }).click()
       await expect(page.getByText('a new blog Playwright test blog by Test user added')).toBeVisible()
     })
+    
+    describe('when logged in and blog created', () => {
+      beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'Add new blog' }).click()
+        await page.getByTestId('title').fill('Creators blog')
+        await page.getByTestId('author').fill('Test user')
+        await page.getByTestId('url').fill('Nourl')
+        await page.getByRole('button', { name: 'Add' }).click()
+      })
+      test('user is blog creator', async ({ page }) => {
+        await page.getByRole('button', { name: 'View' }).click()
+        await expect(page.getByRole('button', { name: 'remove' })).toBeVisible()
+      })
+      test('like button can be clicked', async ({ page }) => {
+        await page.getByRole('button', { name: 'View' }).click()
+        await expect(page.getByRole('button', { name: 'Like' })).toBeVisible()
+        await page.getByRole('button', { name: 'Like '}).click()
+        await expect(page.getByTestId('likes')).toContainText('1')
+      })
+    })
   })
+  
 
 })
