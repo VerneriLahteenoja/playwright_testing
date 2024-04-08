b6ee1e02cd90a67f6abb51a6e99ff0741a451045
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-04-08 19:53:56 +0300

    test create new blog

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,26 +1,49 @@
-const { test, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
-test('front page can be opened', async ({ page }) => {
-  await page.goto('http://localhost:5173')
-  const locator = page.getByText('Blogs')
-  await expect(locator).toBeVisible()
-})
 
-test('can open log in form', async ({ page }) => {
-  await page.goto('http://localhost:5173')
-  await page.getByRole('button', { name: 'Log in' }).click()
-  await expect(page.getByText('username')).toBeVisible()
-  await expect(page.getByText('password')).toBeVisible()
-})
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:5173/api/testing/reset')
+    await request.post('http://localhost:5173/api/users', {
+      data: {
+        username: 'superuser',
+        name: 'Super Tester',
+        password: 'superpass'
+      }
+    })
+    await page.goto('http://localhost:5173')
+  })
 
-test('can log in', async ({ page }) => {
-  await page.goto('http://localhost:5173')
-  await page.getByRole('button', { name: 'Log in' }).click()
-
-  await page.getByTestId('username').fill('superuser')
-  await page.getByTestId('password').fill('superpass')
+  describe('when not logged in', () => {
+    test('front page can be opened', async ({ page }) => {
+      const locator = page.getByText('Blogs')
+      await expect(locator).toBeVisible()
+    })
+    test('can open log in form', async ({ page }) => {
+      await page.getByRole('button', { name: 'Log in' }).click()
+      await expect(page.getByText('username')).toBeVisible()
+      await expect(page.getByText('password')).toBeVisible()
+    })
+  })
   
-  await page.getByRole('button', { name: 'login' }).click()
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'Log in' }).click()
+      await page.getByTestId('username').fill('superuser')
+      await page.getByTestId('password').fill('superpass')
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+    test('can log in', async ({ page }) => {
+      await expect(page.getByText('Super Tester logged in')).toBeVisible()
+    })
+    test('can create new blog', async ({ page }) => {
+      await page.getByRole('button', { name: 'Add new blog' }).click()
+      await page.getByTestId('title').fill('Playwright test blog')
+      await page.getByTestId('author').fill('Test user')
+      await page.getByTestId('url').fill('Nourl')
+      await page.getByRole('button', { name: 'Add' }).click()
+      await expect(page.getByText('a new blog Playwright test blog by Test user added')).toBeVisible()
+    })
+  })
 
-  await expect(page.getByText('Super Tester logged in')).toBeVisible()
 })
